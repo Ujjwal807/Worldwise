4af7de56cd392da47d876b1d4e0c65da82c06a08
Ujjawal <121127299+Ujjwal807@users.noreply.github.com>
2024-04-04 19:49:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import Citylist from "./components/Citylist";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+
+const BASE_URL = "http://localhost:5000";
+
+function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+        <Route index element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities"/>} />
+          <Route
+            path="cities"
+            element={<Citylist cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
